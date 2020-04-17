ecf44a1c2f1296c3f37c194d4242107c96bc2778
Aditya-sairam <37501823+Aditya-sairam@users.noreply.github.com>
2020-04-17 10:40:55 +0530

    Intersection of sorted arrays using sets

---
+++ Intersection.py
@@ -0,0 +1,19 @@
+#Intersection of two sorted arrays
+
+class Solution:
+    def inter(self,set1,set2):
+        return [x for x in set1 if x in set2]
+    
+    
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        set1 = set(nums1)
+        set2 = set(nums2)
+        
+        if(len(set1) < len(set2)):
+            return self.inter(set1,set2)
+        
+        else:
+            return self.inter(set2,set1)
+                
+                
