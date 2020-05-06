243d08738c78325f2424ecc364b37276602ec578
Aditya-sairam <37501823+Aditya-sairam@users.noreply.github.com>
2020-05-06 09:51:48 +0530

    Create Last.py

---
+++ Last.py
@@ -0,0 +1,13 @@
+#Last Stone Weight Problem-Leetcode
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+       
+        if len(stones) ==  1:
+            return stones[0] 
+    
+        while(len(stones) >= 2):
+            stones.sort()
+            stones.append(stones.pop() - stones.pop())
+        
+    
+        return stones[0]
